b1bec6a0f840f055dd51ec9b1a0ef34be34e1c8f
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2024-02-25 19:50:01 -0300

    Update teste_conta.py

--- poo_python-alura/teste_conta.py
+++ poo_python-alura/teste_conta.py
@@ -1,5 +1,10 @@
-from classe_conta import Conta
-
+class Conta:
+    def __init__(self, numero, titular, saldo, limite):
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+      
 conta = Conta()
 
 conta.titular = 'João'
