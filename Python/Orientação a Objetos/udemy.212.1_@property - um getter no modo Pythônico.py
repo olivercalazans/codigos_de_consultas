47cefc430fefe8af0660c85df4ef3a3e8b2e1b4b
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2024-03-17 13:13:29 -0300

    Update udemy.212.1_@property - um getter no modo Pythônico.py
    Adição de informações sobre o decorado que permite que dois ou mais métodos tenham o mesmo nome.

--- Python/Orientação a Objetos/udemy.212.1_@property - um getter no modo Pythônico.py
+++ Python/Orientação a Objetos/udemy.212.1_@property - um getter no modo Pythônico.py
@@ -28,3 +28,25 @@ print(caneta2.cor, caneta2.cor_Da_Tampa)
 print(caneta3.cor, caneta3.cor_Da_Tampa)
 
 # No exemplo acima, a função 'cor' foi chamada como um atributo. Ela não usou os parentese '()'.
+
+
+
+# - Em Python, é possível que dois ou mais métodos tenham o mesmo nome, o que vai diferenca eles é
+#   o decorador.
+# - Um decorador tem que ser o @property, o outro será '<nome_do_metodo>.getter'.
+
+class Caneta:
+    def __init__(self, cor_inicial):
+        self._cor = cor_inicial
+
+    @property
+    def cor(self, nova_cor):
+        self._cor = nova_cor
+    
+    @cor.getter
+    def cor(self):
+        return self._cor
+    
+# No exemplo acima, dois métodos tem o mesmo nome, mas eles tem decoradores diferentes.
+# O método com o decorador @property é um setter. Ele já está definido.
+# O que tem o decorador @cor.getter, ficou assim para que os métodos fosse diferentes.
